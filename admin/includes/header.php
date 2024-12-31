0ccd701f455ca9d29175bb95953ede2591695d07
Ahmed dawood <142524698+Ahmed777525@users.noreply.github.com>
2024-12-31 12:37:14 +0300

    Add files via upload

---
+++ admin/includes/header.php
@@ -0,0 +1,14 @@
+<div class="brand clearfix">
+	<a href="dashboard.php" style="font-size: 20px; padding-top:1%; color:#fff">نظام إدارة بنك الدم والمتبرعين </a>  
+		<span class="menu-btn"><i class="fa fa-bars"></i></span>
+		<ul class="ts-profile-nav">
+			
+			<li class="ts-account">
+				<a href="#"><img src="img/ts-avatar.jpg" class="ts-avatar hidden-side" alt=""> حساب <i class="fa fa-angle-down hidden-side"></i></a>
+				<ul><li><a href="profile.php">حساب تعريفي</a></li>
+					<li><a href="change-password.php">تغيير كلمة المرور</a></li>
+					<li><a href="logout.php">تسجيل الخروج</a></li>
+				</ul>
+			</li>
+		</ul>
+	</div>
---
+++ admin/includes/leftbar.php
@@ -0,0 +1,24 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">رئيسي</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> لوحة التحكم</a></li>
+			
+<li><a href="#"><i class="fa fa-files-o"></i> فصيلة الدم</a>
+<ul>
+<li><a href="add-bloodgroup.php">إضافة فصيلة الدم</a></li>
+<li><a href="manage-bloodgroup.php">إدارة فصيلة الدم</a></li>
+</ul>
+</li>
+
+
+				
+				<li><a href="donor-list.php"><i class="fa fa-users"></i> قائمة المتبرعين</a></li>
+
+				<li><a href="manage-conactusquery.php"><i class="fa fa-desktop"></i> إدارة استفسارات</a></li>
+			<li><a href="manage-pages.php"><i class="fa fa-files-o"></i> إدارة صفحات</a></li>
+			<li><a href="update-contactinfo.php"><i class="fa fa-files-o"></i> تحديث معلومات الاتصال</a></li>
+<li><a href="request-received-bydonar.php"><i class="fa fa-users"></i> البحث عن متبرع</a></li>
+
+			</ul>
+		</nav>
